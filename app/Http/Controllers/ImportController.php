26d884df91d97e8dff3cbcd5de313e13724be889
CuongNV <cuongnv@vtvlive.vn>
2023-02-05 17:59:49 +0700

    update base

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use function Symfony\Component\String\u;
 
 class ImportController extends Controller
 {
@@ -14,8 +17,19 @@ class ImportController extends Controller
         return view('pages.import');
     }
 
-    public function importData(): RedirectResponse {
+    public function importData(Request $request): RedirectResponse {
+        // Check quyen cua user có đc phép inport?
+        if (!$this->checkIsAdmin($request->user)) {
+            throw new HttpException(403);
+        }
+
+        // Gọi job import dữ liệu
+
         // Xử lý import dữ liệu vào các db data
         return Redirect::route('import')->with('status', 'Imported!');
+    }
+
+    private function checkIsAdmin(User $user) {
+        return $user->isAdmin;
     }
 }
